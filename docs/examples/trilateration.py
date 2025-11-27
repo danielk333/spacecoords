62bcfeae83bac72935a2ebf9e437b82c8c694f28
Daniel Kastinen <daniel.kastinen@irf.se>
2025-11-27 09:01:16 +0100

    example done

--- docs/examples/trilateration.py
+++ docs/examples/trilateration.py
@@ -3,16 +3,26 @@ import matplotlib.pyplot as plt
 import spacecoords.linalg as linalg
 
 
+def add_sphere(ax, center, radius, alpha=0.3, res=100):
+    # from matplotlib examples gallery
+    u = np.linspace(0, 2 * np.pi, res)
+    v = np.linspace(0, np.pi, res)
+    x = radius * np.outer(np.cos(u), np.sin(v)) + center[0]
+    y = radius * np.outer(np.sin(u), np.sin(v)) + center[1]
+    z = radius * np.outer(np.ones(np.size(u)), np.cos(v)) + center[2]
+    ax.plot_surface(x, y, z, alpha=alpha)
+
+
 stations = np.stack(
     [
         np.array([1, 0, 0], dtype=np.float64),
-        np.array([2, 1, 0], dtype=np.float64),
-        np.array([0, -1, 0.2], dtype=np.float64),
-        np.array([0, 0, -0.1], dtype=np.float64),
+        np.array([10, 1, 0], dtype=np.float64),
+        np.array([0, -10, 5], dtype=np.float64),
+        np.array([0, 0, -5], dtype=np.float64),
     ],
     axis=1,
 )
-target = np.array([3, 0, 20])
+target = np.array([3, 0, 10])
 
 np.random.seed(328734)
 
@@ -28,22 +38,15 @@ print("x = ", closest_point)
 print("target = ", target)
 print(resid, rank)
 
-# fig = plt.figure(figsize=(15, 15))
-# ax = fig.add_subplot(111, projection="3d")
-#
-# ax.plot(stations[0, :], stations[1, :], stations[2, :], "ob")
-# ax.plot(target[0], target[1], target[2], "or")
-#
-# ax.plot(closest_point[0], closest_point[1], closest_point[2], "xg")
-#
-# for ind in range(stations.shape[1]):
-#     st = stations[:, ind]
-#     ax.plot([target[0], st[0]], [target[1], st[1]], [target[2], st[2]], "--k", alpha=0.3)
-#
-# s_vec = np.linspace(0, 40, 200)
-# for ind in range(stations.shape[1]):
-#     st = stations[:, ind]
-#     x = line(st, dirs_norm[:, ind], s_vec)
-#     ax.plot(x[0, :], x[1, :], x[2, :], "-g")
-#
-# plt.show()
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(111, projection="3d")
+
+ax.plot(stations[0, :], stations[1, :], stations[2, :], "ob")
+ax.plot(target[0], target[1], target[2], "or")
+ax.plot(closest_point[0], closest_point[1], closest_point[2], "xg")
+
+for ind in range(stations.shape[1]):
+    add_sphere(ax, stations[:, ind], ranges[ind])
+
+ax.set_aspect("equal")
+plt.show()
